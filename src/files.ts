fdda89c639089394c20fe5b9fe9e906fad8ac22c
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-07-31 09:21:04 +0200

    #8 Add code to read json config from home directory

---
+++ src/files.ts
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+export const getHomeDir = (): string =>
+  process.platform == 'win32' ? process.env.USERPROFILE : process.env.HOME;
+
+export const readJSON = (
+  absolutPath: string,
+  defaultData: Record<string, any> = {},
+): Record<string, any> => {
+  const rawData = fs.readFileSync(absolutPath, 'utf8');
+  const data = JSON.parse(rawData);
+  return { ...defaultData, ...data };
+};
+
+export const readHomeDirectoryConfig = (
+  filename: string,
+  defaultData: Record<string, any> = {},
+) => {
+  const absolutePath = path.resolve(getHomeDir(), filename);
+  return readJSON(absolutePath, defaultData);
+};
