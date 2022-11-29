82c55bde8d9c78e8d60c7b1a164665596c309211
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2022-11-29 14:51:20 +0100

    Add isomorphic-git
    #39

---
+++ src/git.isomorphic.ts
@@ -0,0 +1,83 @@
+import * as fs from 'fs';
+import * as isoGit from 'isomorphic-git';
+
+import { Commit } from './types';
+import { generateUniquesBasedOnKeyFilter } from './git.utils';
+
+type Props = {
+  gitPath: string;
+  countMerges: boolean;
+  since: 'always' | Date;
+  until: 'always' | Date;
+};
+
+async function getBranchNames(args: Props): Promise<string[]> {
+  return isoGit.listBranches({ fs, dir: args.gitPath });
+}
+
+export async function getCommitsForRepository({
+  gitPath,
+  countMerges,
+  since,
+  until,
+}: Props): Promise<Commit[]> {
+  const branchNames = await getBranchNames({
+    gitPath,
+    countMerges,
+    since,
+    until,
+  });
+
+  const allCommits: Commit[] = [];
+  const branchCommits = await Promise.all(
+    branchNames.map((ref) =>
+      getBranchCommits({ ref, since, until, dir: gitPath }),
+    ),
+  );
+  branchCommits.forEach((commitList) =>
+    commitList.forEach((commit) => allCommits.push(commit)),
+  );
+
+  // Multiple branches might share commits, so take unique
+  const uniqueCommits = allCommits.filter(
+    generateUniquesBasedOnKeyFilter('sha'),
+  );
+
+  if (countMerges) {
+    return uniqueCommits;
+  }
+
+  const isNotMergeCommit = (commit) => !commit.message.startsWith('Merge ');
+  return uniqueCommits.filter(isNotMergeCommit);
+}
+
+async function getBranchCommits({
+  ref,
+  dir,
+  since,
+  until,
+}: {
+  ref: string;
+  dir: string;
+  since: Date | 'always';
+  until: Date | 'always';
+}): Promise<Commit[]> {
+  let commits = await isoGit.log({
+    ref,
+    fs,
+    dir,
+    since: since === 'always' ? undefined : since,
+  });
+  if (until !== 'always') {
+    const maxAge = Math.ceil(until.getTime() / 1000);
+    commits = commits.filter((c) => c.commit.committer.timestamp < maxAge);
+  }
+
+  return commits.map((c) => ({
+    sha: c.oid,
+    author: { email: c.commit.author.email, name: c.commit.author.name },
+    date: new Date(c.commit.committer.timestamp * 1000),
+    message: c.commit.message,
+    repo: dir,
+  }));
+}
